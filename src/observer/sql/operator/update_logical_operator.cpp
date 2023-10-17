5d6ec7244055c7503fc9b39f20fe20185226f943
Michael Xu <xzhseh@gmail.com>
2023-10-17 15:33:56 -0400

    tiny update

--- src/observer/sql/operator/update_logical_operator.cpp
+++ src/observer/sql/operator/update_logical_operator.cpp
@@ -1,4 +1,4 @@
 #include "sql/operator/update_logical_operator.h"
 
-UpdateLogicalOperator::UpdateLogicalOperator(Table *table, const Value& value, int value_offset) : table_(table), value_(value), value_offset_(value_offset) 
-{}
+UpdateLogicalOperator::UpdateLogicalOperator(Table *table, const Value& value, int value_offset)
+    : table_(table), value_(value), value_offset_(value_offset) {}
