49bf3556909475cf16fdab720a8180e77901cd0d
xiaguan <751080330@qq.com>
2023-10-19 12:50:08 +0800

    chore : fix pr comments

--- src/observer/sql/operator/inner_join_physical_operator.h
+++ src/observer/sql/operator/inner_join_physical_operator.h
@@ -9,7 +9,7 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 //
-// Created by WangYunlai on 2021/6/10.
+// Created by susun on 23-10-17.
 //
 
 #pragma once
@@ -27,27 +27,23 @@ public:
   explicit InnerJoinOperator(std::unique_ptr<Expression> join_condition);
   ~InnerJoinOperator() override = default;
 
-  virtual PhysicalOperatorType type() const override
-  {
-    return PhysicalOperatorType::NESTED_LOOP_JOIN;
-  }
+  virtual PhysicalOperatorType type() const override { return PhysicalOperatorType::NESTED_LOOP_JOIN; }
 
-  RC open(Trx *trx) override;
-  RC next() override;
-  RC close() override;
+  RC     open(Trx *trx) override;
+  RC     next() override;
+  RC     close() override;
   Tuple *current_tuple() override;
 
 private:
-
 private:
   Trx *trx_ = nullptr;
 
-  PhysicalOperator *left_ = nullptr;
-  PhysicalOperator *right_ = nullptr;
+  PhysicalOperator           *left_  = nullptr;
+  PhysicalOperator           *right_ = nullptr;
   std::unique_ptr<Expression> join_condition_;
-  Tuple *left_tuple_ = nullptr;
-  Tuple *right_tuple_ = nullptr;
-  JoinedTuple joined_tuple_;  //! 当前关联的左右两个tuple
-  bool round_done_ = true;    //! 右表遍历的一轮是否结束
-  bool right_closed_ = true;  //! 右表算子是否已经关闭
+  Tuple                      *left_tuple_  = nullptr;
+  Tuple                      *right_tuple_ = nullptr;
+  JoinedTuple                 joined_tuple_;         //! 当前关联的左右两个tuple
+  bool                        round_done_   = true;  //! 右表遍历的一轮是否结束
+  bool                        right_closed_ = true;  //! 右表算子是否已经关闭
 };
